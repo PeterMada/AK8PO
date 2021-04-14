1280608b389880a8714308d491211618ed057724
PeterMada <petermada@gmail.com>
2021-04-14 08:10:11 +0200

    Install cros

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -19,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+/*
+app.use(cors({
+  origin: 'http://yourapp.com'
+}));
+*/
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
