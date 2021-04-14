7e0a86eabed8e9cb0f5c1b3a190dd5f376afc833
PeterMada <petermada@gmail.com>
2021-04-14 08:18:38 +0200

    Install react router

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="App-intro">{state.apiResponse}</p>
+
       </header>
     </div>
   );
